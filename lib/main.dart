48a6ca1c39c73c24abd4cafc3e86319b67068b03
Samarth Paliwal <ram.paliwal49@gmail.com>
2019-06-19 08:38:46 +0530

    commented all unnecessary files

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,20 @@
-import 'package:smart_attendance/pages/setup/welcome.dart';
-import 'package:flutter/material.dart';
-import 'package:smart_attendance/globals.dart' as globals;
-
-void main() {
-
-  runApp(MyApp());
-}
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Smart Attendance',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: WelcomePage(),
-    );
-  }
-}
+//import 'package:smart_attendance/pages/setup/welcome.dart';
+//import 'package:flutter/material.dart';
+//import 'package:smart_attendance/globals.dart' as globals;
+//
+//void main() {
+//
+//  runApp(MyApp());
+//}
+//class MyApp extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    return MaterialApp(
+//      title: 'Smart Attendance',
+//      theme: ThemeData(
+//        primarySwatch: Colors.blue,
+//      ),
+//      home: WelcomePage(),
+//    );
+//  }
+//}
--- lib/services/authentication.dart
+++ lib/services/authentication.dart
@@ -1,32 +1,32 @@
-import 'dart:async';
-import 'package:firebase_auth/firebase_auth.dart';
-
-
-class UserData {// here users are made , we need to give it another variable , i.e. role - teacher or student
-  String displayName;
-  String email;
-  String uid;
-  String password;
-
-  UserData({this.displayName, this.email, this.uid, this.password});
-}
-
-class UserAuth {
-  String statusMsg="Account Created Successfully";
-  //To create new User
-  Future<String> createUser(UserData userData) async{
-    FirebaseAuth firebaseAuth = FirebaseAuth.instance;
-    await firebaseAuth
-        .createUserWithEmailAndPassword(
-            email: userData.email, password: userData.password);
-    return statusMsg;
-  }
-
-  //To verify new User
-  Future<String> verifyUser (UserData userData) async{
-    FirebaseAuth firebaseAuth = FirebaseAuth.instance;
-    await firebaseAuth
-        .signInWithEmailAndPassword(email: userData.email, password: userData.password);
-    return "Login Successfull";
-  }
-}
+//import 'dart:async';
+//import 'package:firebase_auth/firebase_auth.dart';
+//
+//
+//class UserData {// here users are made , we need to give it another variable , i.e. role - teacher or student
+//  String displayName;
+//  String email;
+//  String uid;
+//  String password;
+//
+//  UserData({this.displayName, this.email, this.uid, this.password});
+//}
+//
+//class UserAuth {
+//  String statusMsg="Account Created Successfully";
+//  //To create new User
+//  Future<String> createUser(UserData userData) async{
+//    FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+//    await firebaseAuth
+//        .createUserWithEmailAndPassword(
+//            email: userData.email, password: userData.password);
+//    return statusMsg;
+//  }
+//
+//  //To verify new User
+//  Future<String> verifyUser (UserData userData) async{
+//    FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+//    await firebaseAuth
+//        .signInWithEmailAndPassword(email: userData.email, password: userData.password);
+//    return "Login Successfull";
+//  }
+//}
